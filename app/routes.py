fda8fb10aec0ab29cac34401724694af149c55fd
trontzal <trontzal@gmail.com>
2023-07-11 20:10:58 +0200

    primra funcion de pedir usuarios funciiona

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 from app import app
+from app.__init__ import db_connection
 
 @app.route('/')
 def index():
     return "Hola root"
+
+@app.route('/usuarios')
+def get_usuarios():
+    cursor = db_connection.cursor()
+    query = "SELECT * FROM usuarios"
+    cursor.execute(query) 
+    usuarios = cursor.fetchall()
+    cursor.close()
+
+
+    return str(usuarios)
 
 
