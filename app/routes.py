a1711a127139b3271fe9fb0fcc4f57ed9cff05c5
trontzal <trontzal@example.com>
2023-07-23 20:42:28 +0200

    ya se pueden agregar puntuaciones

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from app.__init__ import db_connection
-from flask import request
+from flask import request, jsonify
 
 @app.route('/')
 def index():
@@ -22,17 +22,24 @@ def add_usuario():
     contraseña = request.json.get('contraseña')
 
     cursor = db_connection.cursor()
+
+    # Verificar si el usuario ya existe
+    query = "SELECT COUNT(*) FROM usuarios WHERE id_usuario = %s"
+    cursor.execute(query, (usuario,))
+    count = cursor.fetchone()[0]
+
+    if count > 0:
+        # Si el usuario ya existe, retornar un error
+        return "El usuario ya existe. Por favor, elige otro nombre de usuario.", 409
+
+    # Insertar el nuevo usuario en la base de datos
     query = "INSERT INTO usuarios (id_usuario, contraseña) VALUES (%s, %s)"
     values = (usuario, contraseña)
     cursor.execute(query, values)
     db_connection.commit()
     cursor.close()
 
-    return "Usuario creado exitosamente"
-
-from flask import request, jsonify
-
-from flask import request, jsonify
+    return "Usuario creado exitosamente", 201
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -56,3 +63,54 @@ def login():
             return jsonify({'error': 'Credenciales inválidas'}), 401
     else:
         return jsonify({'error': 'Usuario no encontrado'}), 401
+
+@app.route('/crear_juego', methods=['POST'])
+def crear_juego():
+    try:
+        id_juego = request.json.get('id_juego')
+        nombre_juego = request.json.get('nombre_juego')
+
+        cursor = db_connection.cursor()
+
+        # Consulta SQL para insertar el juego en la tabla juegos
+        query = "INSERT INTO juegos (id_juego, nombre) VALUES (%s, %s)"
+        values = (id_juego, nombre_juego)
+        cursor.execute(query, values)
+
+        db_connection.commit()
+        cursor.close()
+        db_connection.close()
+
+        return jsonify({"message": "Juego creado exitosamente"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/agregar_puntuacion', methods=['POST'])
+def agregar_puntuacion():
+    try:
+        puntuacion = request.json.get('puntuacion')
+        id_usuario = request.json.get('id_usuario')
+        id_juego = request.json.get('id_juego')
+
+        cursor = db_connection.cursor()
+
+        query = "INSERT INTO puntuaciones (puntuacion, id_usuario, id_juego) VALUES (%s, %s, %s)"
+        values = (puntuacion, id_usuario, id_juego)
+        cursor.execute(query, values)
+
+        db_connection.commit()
+        cursor.close()
+
+        return jsonify({"message": "Puntuación agregada exitosamente"}), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/puntuaciones')
+def ver_puntuaciones():
+    cursor = db_connection.cursor()
+    query = "SELECT * FROM puntuaciones"
+    cursor.execute(query) 
+    usuarios = cursor.fetchall()
+    cursor.close()
+
+    return str(usuarios)
